1a349553beb3c0832396ac6faee637a97dfb23f0
Pinakibos23 <97745779+Pinakibos23@users.noreply.github.com>
2022-01-25 20:16:32 +0530

    Recursion
    changed the code from printing hello world to factorials

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+#Resursion
+def recursive_factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * recursive_factorial(n - 1)
+
+num = int(input("Number: "))
+
+if num < 0:
+    print("Number too low!")
+elif num == 0:
+    print("0! = 0")
+else:
+    print(f"{num}! = {recursive_factorial(num)}")
